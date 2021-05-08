3f5c0401be9c161d0f28e7de9aa8a3249d7f1cb2
root <root@thinkeep>
2021-05-08 01:51:10 -0300

    Commit inicial do projeto

---
+++ screens/Login.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { useState } from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Button, Input, Text } from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import styles from '../style/MainStyle';
+
+
+
+export default function Login({navigation}) {
+
+  const [email, setEmail] = useState(null)
+  const [password, setPassword] = useState(null)
+
+  const entrar = () =>{
+
+    navigation.reset({
+      index: 0,
+      routes: [{name: "Principal"}]
+    })
+
+  }
+
+  return (
+      <View style={[styles.container, specificStyle.SpecificContainer]}>
+      <Text h1 >Entrar na CASA DO COREANO</Text>
+      <Input
+        placeholder="E-mail"
+        leftIcon={{ type: 'font-awesome', name: 'envelope' }}
+        onChangeText={value => setEmail(value)}
+        keyboardType="email-address"
+      />
+      <Input
+        placeholder="Sua senha"
+        leftIcon={{ type: 'font-awesome', name: 'lock' }}
+        onChangeText={value => setPassword(value)}
+        secureTextEntry={true}
+      />
+
+      <Button
+  icon={
+    <Icon
+      name="check"
+      size={15}
+      color="white"
+    />
+  }
+  title="Entrar"
+  onPress={ () => entrar ()}
+/>
+
+
+    </View>
+  );
+}
+
+const specificStyle = StyleSheet.create({
+  SpecificContainer:{
+    backgroundColor: '#BEBEBE'
+  }
+})
---
+++ style/MainStyle.js
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#000',
+      alignItems: 'center',
+      justifyContent: 'center',
+      borderColor: '#BEBEBE',
+      borderWidth: 3,
+      borderStyle: 'solid',
+  
+    },
+  });
+
+  export default styles
