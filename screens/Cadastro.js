fa6950cbb67651d1769c78e538c77511bc4c5b74
iury-get-kill <yurii46yuri69@gmail.com>
2021-05-28 07:04:57 -0300

    formulário de cadastro 16-17

--- screens/Cadastro.js
+++ screens/Cadastro.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Button, CheckBox, Input, Text } from 'react-native-elements';
+import { TextInputMask } from 'react-native-masked-text';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import styles from '../style/MainStyle';
 
@@ -13,9 +14,40 @@ export default function Cadastro({navigation}) {
   const [cpf, setCpf] = useState(null)
   const [telefone, setTelefone] = useState(null)
   const [isSelected, setSelected] = useState(false)
+  const [errorEmail, setErrorEmail] = useState(null)
+  const [errorNome, setErrorNome] = useState(null)
+  const [errorCpf, setErrorCpf] = useState(null)
+  const [errorTelefone, setErrorTelefone] = useState(null)
+
+  let cpfField = null
+  let telefoneField = null
+
+  const validar = () => {
+      let error = false
+      setErrorEmail(null)
+      setErrorCpf(null)
+
+      const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      if (!re.test(String(email).toLowerCase())){
+      setErrorEmail ("Preencha seu e-mail corretamente")
+      error = true
+      }
+      if (!cpfField.isValid()){
+        setErrorCpf ("Preencha seu CPF corretamente")
+        error = true
+      }
+      if (telefone == null){
+        setErrorTelefone ("Preencha seu telefone corretamente")
+        error = true
+      }
+      return !error
+  }
+
 
   const salvar = () =>{
+      if (validar()){
       console.log("Salvou")
+      }
   }
 
   return (
@@ -23,28 +55,54 @@ export default function Cadastro({navigation}) {
       <Text h1 >Cadastre-se</Text>
       <Input
         placeholder="E-mail"
-        onChangeText={value => setEmail(value)}
+        onChangeText={value => {
+            setEmail(value)
+            setErrorEmail(null)
+        }}
         keyboardType="email-address"
+        errorMessage={errorEmail}
       />
 
 <Input
         placeholder="Nome"
         onChangeText={value => setNome(value)}
+        errorMessage={errorNome}
       />
 
-<Input
+      <View style={styles.containerMask}>
+        <TextInputMask  
         placeholder="CPF"
-        onChangeText={value => setCpf(value)}
+        type={'cpf'}
+        value={cpf}
+        onChangeText={value => {
+            setCpf(value)
+            setErrorCpf(null)
+        }}
         keyboardType="number-pad"
         returnKeyType="done"
-      />
-
-<Input
+        style={styles.maskedInput}
+        ref={(ref)=> cpfField = ref}
+        />
+        </View>
+      <Text style={styles.errorMessage}>{errorCpf}</Text>
+      <View style={styles.containerMask}>
+        <TextInputMask  
         placeholder="Telefone"
+        type={'cel-phone'}
+        options={{
+          maskType: 'BRL',
+          withDDD: true,
+          dddMask: '(99) '
+        }}
+        value={telefone}
         onChangeText={value => setTelefone(value)}
         keyboardType="phone-pad"
         returnKeyType="done"
-      />
+        style={styles.maskedInput}
+        ref={(ref)=> telefoneField = ref}
+        />
+        </View>
+      <Text style={styles.errorMessage}>{errorTelefone}</Text>
 
       <CheckBox
       title="Eu aceito os termos de uso"
--- style/MainStyle.js
+++ style/MainStyle.js
@@ -11,6 +11,27 @@ const styles = StyleSheet.create({
       borderStyle: 'solid',
   
     },
+    maskedInput:{
+      flexGrow: 1,
+      height: 40,
+      fontSize: 18,
+      borderBottomColor: '#999',
+      borderBottomWidth: 1,
+      borderStyle: "solid",
+      alignSelf: "flex-start"
+    },
+    containerMask:{
+      flexDirection: "row",
+      marginBottom: 5,
+      marginLeft: 10,
+      marginRight: 10
+    },
+    errorMessage: {
+      alignSelf: "flex-start",
+      marginLeft: 15,
+      color: "#f00",
+      fontSize: 12
+    }
   });
 
   export default styles
