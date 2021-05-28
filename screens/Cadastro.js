f56107abdff63992da0696fba3009e454f60d05b
iury-get-kill <yurii46yuri69@gmail.com>
2021-05-28 03:23:53 -0300

    checkBox - 14

--- screens/Cadastro.js
+++ screens/Cadastro.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Button, Input, Text } from 'react-native-elements';
+import { Button, CheckBox, Input, Text } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import styles from '../style/MainStyle';
 
@@ -12,6 +12,7 @@ export default function Cadastro({navigation}) {
   const [nome, setNome] = useState(null)
   const [cpf, setCpf] = useState(null)
   const [telefone, setTelefone] = useState(null)
+  const [isSelected, setSelected] = useState(false)
 
   const salvar = () =>{
       console.log("Salvou")
@@ -43,6 +44,17 @@ export default function Cadastro({navigation}) {
         onChangeText={value => setTelefone(value)}
         keyboardType="phone-pad"
         returnKeyType="done"
+      />
+
+      <CheckBox
+      title="Eu aceito os termos de uso"
+      checkedIcon="check"
+      uncheckedIcon="square-o"
+      checkedColor="green"
+      uncheckedColor="red"
+      checked={isSelected}
+      onPress={()=> setSelected(!isSelected)}
+      
       />
 
       <Button
