bb3caf5be6c4fd6db2f2e934166196098c143124
iury-get-kill <yurii46yuri69@gmail.com>
2021-06-05 12:37:40 -0300

    36

--- style/MainStyle.js
+++ style/MainStyle.js
@@ -1,26 +1,28 @@
 import { StyleSheet } from 'react-native';
-
 const styles = StyleSheet.create({
+    button: {
+      width: "100%",
+      marginTop: 10
+    },  
+    cancelButton: {
+      backgroundColor: "#c00"
+    },
     container: {
       flex: 1,
-      backgroundColor: '#000',
+      backgroundColor: '#ccc',
       alignItems: 'center',
       justifyContent: 'center',
-      borderColor: '#BEBEBE',
-      borderWidth: 3,
-      borderStyle: 'solid',
-  
     },
-    maskedInput:{
+    maskedInput: {
       flexGrow: 1,
       height: 40,
       fontSize: 18,
-      borderBottomColor: '#999',
+      borderBottomColor: "#999",
       borderBottomWidth: 1,
       borderStyle: "solid",
       alignSelf: "flex-start"
     },
-    containerMask:{
+    containerMask: {
       flexDirection: "row",
       marginBottom: 5,
       marginLeft: 10,
@@ -34,4 +36,4 @@ const styles = StyleSheet.create({
     }
   });
 
-  export default styles
+export default styles
